755cd73fd80f4139a43a24a079d6775ff887285a
Ruslan Keba <rukeba@gmail.com>
2020-04-20 16:54:19 +0300

    simple flask web app to inspect url

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, render_template
+
+from inspector import ImgLinkInspector
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    url = request.args.get('url')
+    img_statuses = ImgLinkInspector(url).inspect() if url else None
+    return render_template('index.html', url=url, img_statuses=img_statuses)
