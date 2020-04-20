9510a10919abdc92393a8c416029650f5ba593cf
Ruslan Keba <rukeba@gmail.com>
2020-04-20 17:13:36 +0300

    make app.py able to run

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ from inspector import ImgLinkInspector
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def root():
     url = request.args.get('url')
     img_statuses = ImgLinkInspector(url).inspect() if url else None
     return render_template('index.html', url=url, img_statuses=img_statuses)
+
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
